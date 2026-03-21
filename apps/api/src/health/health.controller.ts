c23cdbc3bce996ae6a56a349be157083b401e461
Chadov Alexey <alexeychadov10@gmail.com>
2026-03-21 23:05:29 +0300

    remove check post

--- apps/api/src/health/health.controller.ts
+++ apps/api/src/health/health.controller.ts
@@ -1,10 +1,5 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
-import {
-  HealthBodyDto,
-  HealthParamsDto,
-  HealthQueryDto,
-} from './dto/health.dto';
 
 @Controller('health')
 export class HealthController {
@@ -12,14 +7,5 @@ export class HealthController {
   @Get()
   health(): { status: string } {
     return this.healthService.check();
-  }
-
-  @Post(':params')
-  postHealth(
-    @Query() query: HealthQueryDto,
-    @Param() params: HealthParamsDto,
-    @Body() body: HealthBodyDto,
-  ): { status: string } {
-    return this.healthService.checkPost(params, query, body);
   }
 }
--- apps/api/src/health/health.service.ts
+++ apps/api/src/health/health.service.ts
@@ -1,20 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import {
-  HealthBodyDto,
-  HealthParamsDto,
-  HealthQueryDto,
-} from './dto/health.dto';
-
 @Injectable()
 export class HealthService {
   check() {
     return { status: 'ok' };
-  }
-  checkPost(
-    params: HealthParamsDto,
-    query: HealthQueryDto,
-    body: HealthBodyDto,
-  ) {
-    return { status: 'ok' + params.params + query.query + body.body };
   }
 }
