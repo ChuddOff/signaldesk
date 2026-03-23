1685872c06f1aca0f7756844d930339bf9591f61
Chadov Alexey <alexeychadov10@gmail.com>
2026-03-23 10:07:13 +0300

    add prisma sevice

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
