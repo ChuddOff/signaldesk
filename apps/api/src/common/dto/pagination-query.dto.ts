a2747493178bc28f385799d173a7dea250693d7e
Chadov Alexey <alexeychadov10@gmail.com>
2026-03-23 13:49:29 +0300

    fix dto

--- apps/api/src/common/dto/pagination-query.dto.ts
+++ apps/api/src/common/dto/pagination-query.dto.ts
@@ -1,14 +1,17 @@
 import { Type } from 'class-transformer';
-import { IsOptional, Min } from 'class-validator';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationQueryDto {
   @Type(() => Number)
+  @IsInt()
   @Min(1)
   @IsOptional()
   p: number = 1;
 
   @Type(() => Number)
+  @IsInt()
   @Min(1)
+  @Max(100)
   @IsOptional()
   per_page: number = 20;
 }
